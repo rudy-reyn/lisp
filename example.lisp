9d64a32fa85665e3b619788bd23d0a40d3a9f370
Rudy Reynolds <rure6748@colorado.edu>
2022-03-09 03:03:13 -0700

    added example program

---
+++ example.lisp
@@ -0,0 +1,22 @@
+;; 03/02/2022
+;; program.lisp
+
+(let even? (=> n (== (% n 2) 0)))
+(let le0? (=> n (<= n 0)))
+(let sqrt (=> n (** n (/ 1 2))))
+
+(let addr (=> x (=> y (+ x y))))
+(let ++10 (addr 10))
+
+(let fibonacci (=> n (
+     (if (< n 2) n
+         (+ (fibonacci (- n 1))
+            (fibonacci (- n 2)))))))
+
+(let ! (=> n
+     (if (le0? n) 1
+         (* n (! (- n 1))))))
+
+(print (fibonacci 10))
+(print (! 10))
+(print (sqrt 2)) (print (sqrt 9))
